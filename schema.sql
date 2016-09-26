11d4befad88a81d8f0d9f055ce9371e323ec2b5e
sdweber422 <stephanweber@Stephans-MacBook-Pro.local>
2016-09-26 15:44:22 -0700

    Added book database and updated schema to match entries from Google Book API

--- schema.sql
+++ schema.sql
@@ -1,9 +1,9 @@
 DROP TABLE IF EXISTS books;
 CREATE TABLE books (
   id SERIAL PRIMARY KEY,
-  title VARCHAR(255) NOT NULL,
-  imageurl VARCHAR(255),
-  description VARCHAR(1000),
+  title VARCHAR(512) NOT NULL,
+  image_url VARCHAR(512),
+  description TEXT,
   purchaseLink VARCHAR(255)
 );
 
