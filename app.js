49c768392a6e95bf54baa31d58f9d48a89363782
nicosesma <nicosm310@gmail.com>
2016-09-26 13:59:33 -0700

    created the routes folder

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ app.set('view engine', 'pug')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended : true }))
 
-app.get('/', (request, response, next) => {
-  response.render('home')
 
-})
+const index = require('./routes/index');
+
+app.use('/', index);
 
 app.listen(3000, function () {
   console.log('Listening on port 3000')
