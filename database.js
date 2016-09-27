423eec6851231977f4040980d4e55445351d0f9e
Douglas Dunn <douglas.dunn@outlook.com>
2016-09-27 13:25:23 -0700

    created navbar, admin outline, and started on details

--- database.js
+++ database.js
@@ -9,6 +9,18 @@ const getAllBooks = page => {
     // .then()
 }
 
+const getBookById = bookId => {
+  return db.one("SELECT * FROM books WHERE id = $1", [bookId])
+}
+
+const getAuthorByBookId = bookId => {
+  let authorId = db.one("SELECT * FROM book_authors WHERE book_id = $1", [bookId])
+  console.log("authorId: "+ authorId)
+  return db.one("SELECT * FROM authors WHERE id = $1", [authorId])
+}
+
 module.exports = {
-  getAllBooks: getAllBooks
+  getAllBooks,
+  getBookById,
+  getAuthorByBookId
 }
